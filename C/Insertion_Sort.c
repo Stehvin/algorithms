270d752864e7797073dd913ea6ef32e108c8501c
Stehvin <stehvin.olson@gmail.com>
2017-09-27 19:54:09 -0500

    Add array printing function

--- C/Insertion_Sort.c
+++ C/Insertion_Sort.c
@@ -1,7 +1,19 @@
 #include <stdio.h>  
 
+void printArr(int *arr);
+
 int main()  
 {  
-    printf("Hello World\n");  
+	int arr[10] = {43, 2, 56, 45, 3, 3, 67, 32, 2, 68};
+	printArr(&arr);
     return 0;  
+}
+
+void printArr(int *arr)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 }
