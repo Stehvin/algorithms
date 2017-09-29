76c2372133c53f64cf74046fdfe71c702a9efa72
Stehvin <stehvin.olson@gmail.com>
2017-09-28 20:59:13 -0500

    Implement insertion sort function

--- C/Insertion_Sort.c
+++ C/Insertion_Sort.c
@@ -1,12 +1,46 @@
 #include <stdio.h>  
 
 void printArr(int *arr);
+void insertionSort(int *arr);
 
 int main()  
 {  
-	int arr[10] = {43, 2, 56, 45, 3, 3, 67, 32, 2, 68};
+	// initialize array and length of array
+	int arr[] = {43, 2, 56, 45, 3, 3, 67, 32, 2, 68};
+	int n = sizeof(arr) / sizeof(int);
+
+	printArr(&arr);
+	insertionSort(arr);
 	printArr(&arr);
     return 0;  
+}
+
+void insertionSort(int *arr, int n)
+{
+	int i, j, cur;
+
+	// place each element of array
+	for (i = 1; i < n; i++)
+	{
+		// value of current element
+		cur = arr[i];
+
+		// move elements bigger than cur up one position,
+		// and place cur just after the first element it is greater than
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (arr[j] > cur)
+			{
+				arr[j + 1] = arr[j];
+				printf("%d\n", cur);
+			}
+			else
+			{
+				break;
+			}
+		}
+		arr[j + 1] = cur;
+	}
 }
 
 void printArr(int *arr)
