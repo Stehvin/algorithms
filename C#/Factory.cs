84c349f6279049a2a6e751d52388e4759f2ca56b
Stehvin <stehvin.olson@gmail.com>
2017-11-16 18:50:59 -0600

    Begin creating factory design pattern layout

---
+++ C#/Factory.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Factory
+{
+    class basketBall
+    {
+
+    }
+
+    class soccerBall
+    {
+
+    }
+
+    class baseBall
+    {
+
+    }
+
+    class Factory
+    {
+
+    }
+
+	class Program
+	{
+		static void Main()
+        {
+            // Test object creation using factory
+		}
+	}
+}
