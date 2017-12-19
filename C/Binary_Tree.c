f516664ac810da8c14e77f0d034b635ee18a2f27
Stehvin <stehvin.olson@gmail.com>
2017-12-18 21:03:48 -0600

    Create tree printing function and test implementation

--- C/Binary_Tree.c
+++ C/Binary_Tree.c
@@ -16,10 +16,62 @@ Node* newNode(int val)
 	new->value = val;
 	new->left = NULL;
 	new->right = NULL;
+	return new;
+}
+
+// Print the tree
+void printTree(Node* root)
+{
+	// Initialize child node checking variables
+	int lCheck = 0;
+	int rCheck = 0;
+
+	// Check left and right nodes
+	for (int i = 0; i < 2; i++)
+	{
+		// Print left node's value, and the current node's value in brackets
+		if (i == 0 && root->left != NULL)
+		{
+			printf("%d [%d] ", root->left->value, root->value);
+			lCheck = 1;
+		}
+
+		// Print right node's value, and the current node's value in brackets
+		else if (i == 1 && root->right != NULL)
+		{
+			printf("%d [%d]  \n", root->right->value, root->value);
+			rCheck = 1;
+		}
+	}
+
+	// Recursively print all child nodes from left and right nodes
+	if (lCheck == 1)
+	{
+		printTree(root->left);
+	}
+	if (rCheck == 1)
+	{
+		printTree(root->right);
+	}
 }
 
 // Test the implementation.
 int main()
 {
+	// Initialize tree
+	Node* root = malloc(sizeof(Node));
+	root->value = 10;
+
+	// Add nodes to root
+	root->left = newNode(9);
+	root->right = newNode(8);
+
+	// Add leaves to second-level nodes
+	root->right->left = newNode(7);
+	root->right->right = newNode(6);
+	root->left->left = newNode(5);
+
+	// Print the tree
+	printTree(root);
 	return 0;
 }
